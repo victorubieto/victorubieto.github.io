b8fee2617c2d9eafcb7bcc2f91c73b64b7cf524a
victorubieto <59796230+victorubieto@users.noreply.github.com>
2026-03-25 15:17:28 +0000

    Deploying to gh-pages from @ victorubieto/victorubieto.github.io@2330d10566f12b78f67f3b1efe06f44bd08b7070 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -62,28 +62,6 @@ ninja.data = [{
             window.location.href = "/blog/2026/spherical-fibonacci-grids/";
           
         },
-      },{id: "post-volumetric-rendering",
-        
-          title: "Volumetric Rendering",
-        
-        description: "lore ipsum",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2015/disqus-comments/";
-          
-        },
-      },{id: "post-a-post-with-math",
-        
-          title: "a post with math",
-        
-        description: "an example of a blog post with some math",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2015/math/";
-          
-        },
       },{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
